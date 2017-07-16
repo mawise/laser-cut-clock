be924fc84fc31705a2c333eecbfa765049c5842b
Matthew Wise <matthew.rs.wise@gmail.com>
2017-07-16 08:55:09 -0700

    update the escapement anchor

--- anchor.rb
+++ anchor.rb
@@ -2,9 +2,15 @@ require './shapes.rb'
 
 def draw_anchor(cx, cy)
   points = [
-    [cx, cy],
-    [cx+13.45, cy+17],
-    [cx-18.85, cy+17]
+    [13.45, 17],
+#    [-18.85, 17],
+    [26,3],
+    [0, -6],
+    [-32, 4],
+    [-18.85, 17],
+    [-23, 6],
+    [18, 6]
   ]
-  polygon(points, "mm", Style::LINE)
+  centered_points = points.map{|p| [cx + p[0], cy + p[1]]}
+  polygon(centered_points, "mm", Style::LINE)
 end
